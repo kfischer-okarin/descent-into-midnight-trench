1705bf0817850c134b39542e3538caf955ae3527
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2021-04-24 19:55:21 +0900

    Switch to entity

--- game/app/main.rb
+++ game/app/main.rb
@@ -22,11 +22,16 @@ end
 module Game
   class << self
     def setup(args)
-      args.state.player = { position: [180, 140], v: [0, 0], x_forward: 1 }
+      args.state.player = args.state.new_entity_strict(
+        :player,
+        position: [180, 140],
+        v: [0, 0],
+        x_forward: 1
+      )
     end
 
     def player_rect(args)
-      position = player(args)[:position]
+      position = player_position(args)
       { x: position.x - 11, y: position.y, w: 21, h: 12 }
     end
 
@@ -42,7 +47,7 @@ module Game
     end
 
     def player_position(args)
-      args.state.player[:position]
+      player(args).position
     end
 
     private
@@ -52,21 +57,21 @@ module Game
     def do_swim(args, input_events)
       horizontal_movement = input_events[:horizontal]
       player = player(args)
-      player[:v].y += 0.5 if input_events[:swim_up]
-      player[:v].x = horizontal_movement * 0.5
-      player[:x_forward] = horizontal_movement.sign unless horizontal_movement.zero?
+      player.v.y += 0.5 if input_events[:swim_up]
+      player.v.x = horizontal_movement * 0.5
+      player.x_forward = horizontal_movement.sign unless horizontal_movement.zero?
     end
 
     def apply_gravity(args)
       player = player(args)
-      player[:v].y -= 0.01
+      player.v.y -= 0.01
     end
 
     def apply_velocity(args)
       player = player(args)
-      clamp_vector(player[:v], MAX_V)
-      player[:position].x += player[:v].x
-      player[:position].y += player[:v].y
+      clamp_vector(player.v, MAX_V)
+      player.position.x += player.v.x
+      player.position.y += player.v.y
     end
 
     def keep_player_inside_screen(args)
@@ -111,7 +116,7 @@ module Render
         source_x: args.tick_count.idiv(10) % 2 == 0 ? 0 : 21,
         source_w: 21,
         source_h: 12,
-        flip_horizontally: player[:x_forward] < 0
+        flip_horizontally: player.x_forward < 0
       ).sprite
       render_target.primitives << base.merge(source_y: 12).merge(args.state.palette[0])
       render_target.primitives << base.merge(source_y: 0).merge(args.state.palette[4])
